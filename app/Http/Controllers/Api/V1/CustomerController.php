8bdcc9cca9fc4ddc3bf3606ac6e968678f4b36ef
oriana <oriana.nahir.r@gmail.com>
2024-05-30 15:46:58 +0200

    'updating'

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\CustomerResource;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Filters\V1\CustomersFilter;
+use App\Http\Requests\V1\StoreCustomerRequest;
+use App\Http\Requests\V1\UpdateCustomerRequest;
 
 class CustomerController extends Controller
 {
@@ -40,5 +42,16 @@ class CustomerController extends Controller
         }
 
         return new CustomerResource($customer);
+    }
+
+    public function store(StoreCustomerRequest $request)
+    {
+        return new CustomerResource(Customer::create($request->all()));
+    }
+
+
+    public function update(UpdateCustomerRequest $request, Customer $customer)
+    {
+        $customer->update($request->all());
     }
 }
