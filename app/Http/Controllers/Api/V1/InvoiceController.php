9d90690bceaa3a066a0012d38a799b703e93a73c
oriana <oriana.nahir.r@gmail.com>
2024-05-30 16:07:03 +0200

    bulk invoices implemented

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -8,6 +8,8 @@ use App\Models\Invoice;
 use App\Http\Resources\V1\InvoiceResource;
 use App\Http\Resources\V1\InvoiceCollection;
 use App\Filters\V1\InvoicesFilter;
+use App\Http\Requests\V1\BulkStoreInvoicesRequest;
+use Illuminate\Support\Arr;
 
 class InvoiceController extends Controller
 {
@@ -29,5 +31,15 @@ class InvoiceController extends Controller
     public function show(Invoice $invoice)
     {
         return new InvoiceResource($invoice);
+    }
+
+    public function bulkStore(BulkStoreInvoicesRequest $request)
+    {
+       $bulk = collect($request->all())->map(function ($arr, $key)
+       {
+        return Arr::except($arr, ['customerId', 'billedDate', 'paidDate']);
+       });
+
+       Invoice::insert($bulk->toArray());
     }
 }
