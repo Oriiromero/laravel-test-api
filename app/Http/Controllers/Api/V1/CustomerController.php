a42a54e16f12adcf46f386d6a7f3e288ba2e0229
oriana <oriana.nahir.r@gmail.com>
2024-05-30 11:25:29 +0200

    'filerting'

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -5,11 +5,40 @@ namespace App\Http\Controllers\Api\V1;
 use App\Models\Customer;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\CustomerResource;
+use App\Http\Resources\V1\CustomerCollection;
+use App\Filters\V1\CustomersFilter;
 
 class CustomerController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return Customer::all();
+        $filter= new CustomersFilter();
+        $filterItems = $filter->transform($request);
+
+        $includeInvoices = $request->query('includeInvoices');
+
+        $customers = Customer::where($filterItems);
+        
+        if( $includeInvoices)
+        {
+            $customers = $customers->with('invoices');
+        }
+        
+        return new CustomerCollection($customers->paginate()->appends($request->query()));
+
+    }
+
+    public function show(Customer $customer)
+    {
+
+        $includeInvoices = request()->query('includeInvoices');
+
+        if($includeInvoices) 
+        {
+            return new CustomerResource($customer->loadMissing('invoices'));
+        }
+
+        return new CustomerResource($customer);
     }
 }
--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -5,11 +5,29 @@ namespace App\Http\Controllers\Api\V1;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Invoice;
+use App\Http\Resources\V1\InvoiceResource;
+use App\Http\Resources\V1\InvoiceCollection;
+use App\Filters\V1\InvoicesFilter;
 
 class InvoiceController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return Invoice::all();
+        $filter= new InvoicesFilter();
+        $queryItems = $filter->transform($request);
+
+        if(count($queryItems) == 0)
+        {
+            return new InvoiceCollection(Invoice::paginate());
+        }else
+        {
+            $invoices = Invoice::where($queryItems)->paginate();
+            return new InvoiceCollection($invoices->appends($request->query()));
+        }
+    }
+
+    public function show(Invoice $invoice)
+    {
+        return new InvoiceResource($invoice);
     }
 }
